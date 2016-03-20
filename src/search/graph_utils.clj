f7fdace9c34de81e021c1d7fd1bf015aac4caa99
Saul Shanabrook <s.shanabrook@gmail.com>
2016-03-20 17:26:59 -0400

    Add profiling wrapper

--- src/search/graph_utils.clj
+++ src/search/graph_utils.clj
@@ -1,8 +1,10 @@
 (ns search.graph-utils
   (:require [schema.core :as s]
             [plumbing.core :refer [fnk]]
+            [taoensso.timbre.profiling :as profiling]
 
-            [search.utils :as utils]))
+            [search.utils :as utils]
+            [search.core :as search]))
 
 (defn println-level
   "Like println but indents by `indent` first."
@@ -25,3 +27,23 @@
           (utils/wrap-before (fn [& _] (println-level @indent "Calling" ks) (swap! indent inc)))
           (utils/wrap-after (fn [_] (swap! indent dec) (println-level @indent "Finished" ks)))))
       g)))
+
+(def profile-fns-wrap
+  "Add timbre profile to each function"
+  (partial utils/map-leaf-fns
+    (fnk profile-wrap-inner [f ks]
+      (utils/wrap f (fn [& args] (profiling/p (str ks) (apply f args)))))))
+
+(s/defn print-profile-gen-wrap :- search/SearchGraph
+  "Prints the total profiling after each generation"
+  [{->gens :generations :as g} :- search/SearchGraph]
+  (assoc g :generations
+    (utils/wrap ->gens
+      (fn [m]
+        (let [f (fn profile-fns-inner [gens]
+                  (let [first-g (profiling/profile :info :Generation (first gens))]
+                    (if (nil? first-g)
+                      nil
+                      (lazy-seq (cons first-g
+                                      (profile-fns-inner (rest gens)))))))]
+          (f (->gens m)))))))
